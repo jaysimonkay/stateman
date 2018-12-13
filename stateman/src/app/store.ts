08bc76fd1b76c37b5d0c41c6bbb2e193797a71eb
Jude Kuti <judekuti@gmail.com>
2018-12-13 17:20:42 -0600

    Create action cases using a switch statement

--- stateman/src/app/store.ts
+++ stateman/src/app/store.ts
@@ -1,4 +1,10 @@
 import { ITodo } from './todo';
+import {
+  ADD_TODO,
+  TOGGLE_TODO,
+  REMOVE_TODO,
+  REMOVE_ALL_TODOS
+} from './actions';
 
 export interface IAppState {
   todos: ITodo[];
@@ -9,5 +15,16 @@ export const INITIAL_STATE: IAppState = {
   lastUpdate: null
 };
 export function rootReducer(state, action) {
+  switch (action.type) {
+    case ADD_TODO:
+      action.todo.id = state.todos.length + 1;
+      return Object.assign({}, state, {
+        todos: state.todos.concat(Object.assign({}, action.todo)),
+        lastUpdate: new Date()
+      });
+    case TOGGLE_TODO:
+    case REMOVE_TODO:
+    case REMOVE_ALL_TODOS:
+  }
   return state;
 }
